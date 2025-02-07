417b2857b09214af96a4086d000a35c3f884ed1e
Flavio Piccoli <flavio.piccoli@unimib.it>
2025-02-07 18:23:02 +0100

    Improved transformations.

--- CNNTrainer.py
+++ CNNTrainer.py
@@ -6,10 +6,15 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torch.utils.data as data
 import pytorch_lightning as pl
+from functools import partial
 from torchvision import transforms, models
 from pytorch_lightning.callbacks import RichProgressBar, ModelCheckpoint
 from PIL import Image
 from tqdm.rich import tqdm
+import numpy as np
+import cv2
+from einops import rearrange
+import albumentations as A
 
 
 class ImageDataset(torch.utils.data.Dataset):
@@ -24,7 +29,7 @@ class ImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         image = Image.open(self.image_paths[idx]).convert("RGB")
         if self.transform:
-            image = self.transform(image)
+            image = self.transform(image=image)
         label = self.labels[idx]
         return image, label
 
@@ -72,15 +77,90 @@ class CNNTrainer(pl.LightningModule):
     
 
 
+
+
+
+def transform_fun(train, sz, image):
+    # if it is a pil image, transform to numpy array
+    if isinstance(image, Image.Image):
+        image = np.array(image)
+    # TODO: set random transforms when train_transforms is True
+    # if train:
+    #     # if training, randomly apply some transforms
+    #     trans = A.Compose([
+    #         A.RandomRotate90(p=0.3),
+    #         A.Flip(p=0.3),
+    #         A.Transpose(p=0.3),
+    #         A.GaussNoise(p=0.1),
+    #         A.OneOf([
+    #             A.MotionBlur(p=.2),
+    #             A.MedianBlur(blur_limit=3, p=0.1),
+    #             A.Blur(blur_limit=3, p=0.1),
+    #         ], p=0.2),
+    #         A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
+    #         A.OneOf([
+    #             A.OpticalDistortion(p=0.3),
+    #             A.GridDistortion(p=.1),
+    #         ], p=0.2),
+    #         A.OneOf([
+    #             A.CLAHE(clip_limit=2),
+    #             A.RandomBrightnessContrast(),
+    #         ], p=0.3),
+    #         A.HueSaturationValue(p=0.1),
+    #         A.LongestMaxSize(max_size=self.sz),
+    #         A.PadIfNeeded(min_height=self.sz, min_width=self.sz, border_mode=cv2.BORDER_CONSTANT, value=.0, mask_value=.0),
+    #         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0),
+    #   ])
+    if train:
+        # in this case, we want to apply only geometric transforms
+        trans = A.Compose([
+            A.RandomRotate90(p=0.3),
+            A.Flip(p=0.3),
+            A.Transpose(p=0.3),
+            A.GaussNoise(p=0.1),
+            A.OneOf([
+                A.MotionBlur(p=.2),
+                A.MedianBlur(blur_limit=3, p=0.1),
+                A.Blur(blur_limit=3, p=0.1),
+            ], p=0.2),
+            A.OneOf([
+                A.OpticalDistortion(p=0.3),
+                A.GridDistortion(p=.1),
+            ], p=0.2),
+            A.LongestMaxSize(max_size=sz),
+            A.PadIfNeeded(min_height=sz, min_width=sz, border_mode=cv2.BORDER_CONSTANT, value=.0, mask_value=.0),
+            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0),
+        ])
+    else:
+        # define transform
+        trans = A.Compose([
+                A.LongestMaxSize(max_size=sz),
+                A.PadIfNeeded(min_height=sz, min_width=sz, border_mode=cv2.BORDER_CONSTANT, value=.0, mask_value=.0),
+                A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225), max_pixel_value=255.0),
+        ])
+
+    # apply it
+    image_t = trans(image=image)['image']
+    # change order
+    # image_t = rearrange(image_t, 'h w c -> 1 c h w')
+    image_t = rearrange(image_t, 'h w c -> c h w')
+    # convert to tensor
+    image_t = torch.tensor(image_t, dtype=torch.float32)
+    # return
+    return image_t
+
+
+
     
 
 
 def train_cnn(image_paths, labels, model_name, epochs, learning_rate, batch_size, num_classes, pretrained=True):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    # transform = transforms.Compose([
+    #     transforms.Resize((224, 224)),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    # ])
+    transform = partial(transform_fun, train=True, sz=256)
     dataset = ImageDataset(image_paths, labels, transform)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     
@@ -99,11 +179,12 @@ def train_cnn(image_paths, labels, model_name, epochs, learning_rate, batch_size
 
 
 def classify_with_cnn(image_paths, model):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    # transform = transforms.Compose([
+    #     transforms.Resize((224, 224)),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    # ])
+    transform = partial(transform_fun, train=False, sz=256)
     
     model.eval()
     # predictions = {}
@@ -111,7 +192,7 @@ def classify_with_cnn(image_paths, model):
     
     for image_path in tqdm(image_paths):
         image = Image.open(image_path).convert("RGB")
-        image = transform(image).unsqueeze(0)  # Add batch dimension
+        image = transform(image=image).unsqueeze(0)  # Add batch dimension
         
         with torch.no_grad():
             output = model(image)
