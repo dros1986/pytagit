dd2c61b295716047c7903f4d981b69c419ca4c36
Flavio Piccoli <flavio.piccoli@unimib.it>
2025-06-06 11:31:58 +0200

    Updated window name.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytagit",
-    version="0.1.4",
+    version="0.1.5",
     author="Flavio Piccoli",
     author_email="dros1986@gmail.com",
     description="Interactive tool for image tagging with the human in the loop",
